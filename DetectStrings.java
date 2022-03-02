d3880145970d45f181a919123aef5c8f3f28261e
Luke Holt <holt.74@wright.edu>
2022-03-02 17:19:19 -0500

    adding a few more search terms

--- DetectStrings.java
+++ DetectStrings.java
@@ -9,11 +9,13 @@ import java.util.Arrays;
 public class DetectStrings extends BaseScript {
 	
 	// TODO: add additional search terms
-	public static ArrayList<String> SearchStrings = new ArrayList<String>(Arrays.asList("Sandbox"));
+    public static ArrayList<String> SearchStrings = new ArrayList<String>(Arrays.asList("Sandbox", "sand box", "vm", "vbox", "virtualbox"));
 	
 	@Override
 	protected void run() throws Exception {
-		
+
+	    super.run();
+	    
 		for(String term: SearchStrings) {
 			Address foundAddr = find(term);
 			if(foundAddr != null)
