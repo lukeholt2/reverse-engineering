c1f27c659137a2a464f50b5d0eaed28c9eba9078
Luke Holt <holt.74@wright.edu>
2022-03-02 17:59:05 -0500

    commenting analyze.sh + setting log path

--- analyze.sh
+++ analyze.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-
 BINARY="$1"
-
 
 if [ -z "$BINARY" ]; then
     echo 'Binary Required'
@@ -16,7 +14,13 @@ GHIDRA=$(find ~/ -type f -name 'analyzeHeadless')
 # specify the directory containing custom ghidra scripts
 SCRIPTPATH=~/reverse-engineering/
 
-
-$GHIDRA . tmp_proj -import $BINARY -deleteProject -scriptPath $SCRIPTPATH -postScript ./Detection.java
+# run the ghidra 'analyzeHeadless' with the settings:
+# . - use current directory
+# tmp_proj - project name; use temp name as it will be deleted after
+# -deleteProject - self-explanatory, delete the project when finished
+# -scriptPath - search path to ensure scripts are found
+# -postScript - specifies script to run
+# -scriptlog - where to log output from scripts (ignoring the other clutter outputted by ghidra)
+$GHIDRA . tmp_proj -import $BINARY -deleteProject -scriptPath $SCRIPTPATH -postScript ./Detection.java -scriptlog ./results.log
 
 
